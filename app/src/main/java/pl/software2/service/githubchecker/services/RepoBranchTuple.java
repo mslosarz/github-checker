bcf6ca8abb1ab45b54e2a5f58033309795a784d2
Marcin Ślósarz <m.slosarz@ahej.pl>
2023-09-25 17:27:40 +0200

    removed redundant classes

--- app/src/main/java/pl/software2/service/githubchecker/services/RepoBranchTuple.java
+++
@@ -1,7 +0,0 @@
-package pl.software2.service.githubchecker.services;
-
-import pl.software2.service.githubchecker.model.github.RepoBranch;
-import pl.software2.service.githubchecker.model.github.UserRepo;
-
-public record RepoBranchTuple(UserRepo repo, RepoBranch branch) {
-}
