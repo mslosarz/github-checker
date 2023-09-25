cec9783d348c7bf49e7367256ff32ce4afea7d75
Marcin Ślósarz <m.slosarz@ahej.pl>
2023-09-25 19:44:17 +0200

    Added health check

---
+++ app/src/main/java/pl/software2/service/githubchecker/controllers/HcController.java
@@ -0,0 +1,21 @@
+package pl.software2.service.githubchecker.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController("/")
+public class HcController {
+
+
+    @Autowired
+    private HcController() {
+    }
+
+    @GetMapping(value = "/hc", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getUserRepositories() {
+        return Mono.just("Ok");
+    }
+}
